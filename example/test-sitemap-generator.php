e85e90a189b0bd8acf69bab299cfe08978579957
fayyaztech <fayyaztech@gmail.com>
2025-04-13 14:22:54 +0530

    🔧 fix(test-sitemap-generator): update domain and autoloading for SitemapGenerator

--- example/test-sitemap-generator.php
+++ example/test-sitemap-generator.php
@@ -1,15 +1,14 @@
 <?php
 
 // Include the SitemapGenerator class
-require_once '../src/SitemapGenerator.php';
-require_once '../src/ChangeFrequency.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 // Use the SitemapGenerator class
 use Fayyaztech\SitemapGenerator\ChangeFrequency;
 use Fayyaztech\SitemapGenerator\SitemapGenerator;
 
 // Instantiate the SitemapGenerator class with a custom domain (optional)
-$sitemapGenerator = new SitemapGenerator(ChangeFrequency::MONTHLY, 'http://localhost:8080');
+$sitemapGenerator = new SitemapGenerator(ChangeFrequency::MONTHLY, 'https://www.drbajrcollege.org');
 
 // Alternatively, without providing a domain (it will use the current domain)
 // $sitemapGenerator = new SitemapGenerator();
